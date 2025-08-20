130353ad3add385dd18e6e01f2bedb418da8157d
EnderSpy29 <theenderspy29@gmail.com>
2025-08-20 21:39:51 +1200

    limit FPS

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def main():
     game_loop = True
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clockers = pygame.time.Clock()
+    dt = 0
     while game_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Black)
         pygame.display.flip()
+        dt = Clockers.tick(60) / 1000
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
