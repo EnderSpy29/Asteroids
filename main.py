2d5a5379a73de3785c8aa6869de696e82ef9a592
EnderSpy29 <theenderspy29@gmail.com>
2025-08-21 20:12:03 +1200

    add player1 and render it

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     Black = 000000
     game_loop = True
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     Clockers = pygame.time.Clock()
     dt = 0
+
+    player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while game_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Black)
+        player1.draw(screen)
         pygame.display.flip()
         dt = Clockers.tick(60) / 1000
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
