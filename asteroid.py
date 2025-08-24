36dc9cece8eb69280cb9caf11fc42a246bb03879
EnderSpy29 <theenderspy29@gmail.com>
2025-08-24 23:15:14 +1200

    fixed asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -3,13 +3,14 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super().__init__(x,y,radius)
+
         self.x = x
         self.y = y
         self.radius = radius
-        self.position = 0
 
     def draw(self,screen):
-        pygame.draw.circle(screen,"red",[self.x,self.y],self.radius,width=2)
+        pygame.draw.circle(screen,'red',self.position,self.radius,width=2)
 
     def update(self,dt):
         self.position += self.velocity * dt
